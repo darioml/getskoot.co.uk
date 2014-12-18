b0495586b3c0f506d2ad9dd9b7b3dad7383018a7
= <dario_dml@hotmail.com>
2014-12-18 19:25:29 +0000

    Fixed parralax and navigation on mobile

--- page.js
+++ page.js
@@ -1,7 +1,6 @@
 $(function() {
 	$( window ).scroll(function() {
 		var dist = window.pageYOffset || document.documentElement.scrollTop;
-		console.log(dist)
 
 		if (dist > 60) {
 			$(".header").addClass("small");
@@ -9,27 +8,37 @@ $(function() {
 			$(".header").removeClass("small");
 		}
 	});
+
+	var makeHeight = function() {
+		if ($window.width() < 600) {
+			$('html').css('height', window.innerHeight)
+			$('body').css('height', window.innerHeight)
+		}
+	}
 })
 
 
 $(document).ready(function(){
    // cache the window object
    $window = $(window);
- 
-   $('[data-type="background"]').each(function(){
-     // declare the variable to affect the defined data-type
-     var $scroll = $(this);
-                     
-      $(window).scroll(function() {
-        // HTML5 proves useful for helping with creating JS functions!
-        // also, negative value because we're scrolling upwards                             
-        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-         
-        // background position
-        var coords = '50% '+ yPos + 'px';
- 
-        // move the background
-        $scroll.css({ backgroundPosition: coords });    
-      }); // end window scroll
-   });  // end section function
+
+
+   if ($window.width() > 600) {
+	   $('[data-type="background"]').each(function(){
+	     // declare the variable to affect the defined data-type
+	     var $scroll = $(this);
+	                     
+	      $(window).scroll(function() {
+	        // HTML5 proves useful for helping with creating JS functions!
+	        // also, negative value because we're scrolling upwards                             
+	        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+	         
+	        // background position
+	        var coords = '50% '+ yPos + 'px';
+	 
+	        // move the background
+	        $scroll.css({ backgroundPosition: coords });    
+	      }); // end window scroll
+	   });  // end section function
+	}
 }); // close out script
